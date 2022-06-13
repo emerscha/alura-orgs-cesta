6373abc48c1e8e7c9ee5906c8118adad189bbfec
Emerson Schafhauser <emerson.scha@gmail.com>
2022-06-13 17:47:31 -0300

    Refatoração do Código

---
+++ src/components/CustomText.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+
+export default function CustomText({ children, style }) {
+  let defaultStyle = styles.text;
+
+  if (style?.fontWeight === "bold") {
+    defaultStyle = styles.boldText;
+  }
+  return <Text style={[style, defaultStyle]}>{children}</Text>;
+}
+
+const styles = StyleSheet.create({
+  text: {
+    fontFamily: "MontserratRegular",
+    fontWeight: "normal",
+  },
+  boldText: {
+    fontFamily: "MontserratBold",
+    fontWeight: "normal",
+  },
+});
---
+++ src/mocks/basket.js
@@ -0,0 +1,17 @@
+import logo from "../../assets/logo.png";
+
+const basket = {
+  top: {
+    title: "Detalhes da Cesta",
+  },
+  details: {
+    basketName: "Cesta de Verduras",
+    logoFarm: logo,
+    farmName: "Jenny Jack Farm",
+    description:
+      "Uma cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha",
+    price: "R$ 50,00",
+  },
+};
+
+export default basket;
