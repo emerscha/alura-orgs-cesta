749d752ae8f8627108f8330ca6eb4fce6ee7eb19
Emerson Schafhauser <emerson.scha@gmail.com>
2022-06-13 18:04:24 -0300

    Alteração do AppLoading por SplashScreen

--- App.js
+++ App.js
@@ -1,23 +1,26 @@
-import { StatusBar, SafeAreaView, View } from "react-native";
+import { StatusBar, SafeAreaView, View } from 'react-native'
 import {
   useFonts,
   Montserrat_400Regular,
-  Montserrat_700Bold,
-} from "@expo-google-fonts/montserrat";
+  Montserrat_700Bold
+} from '@expo-google-fonts/montserrat'
 
-import AppLoading from "expo-app-loading";
+import * as SplashScreen from 'expo-splash-screen'
 
-import Basket from "./src/screens/basket";
-import mock from "./src/mocks/basket";
+import Basket from './src/screens/basket'
+import mock from './src/mocks/basket'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     MontserratRegular: Montserrat_400Regular,
-    MontserratBold: Montserrat_700Bold,
-  });
+    MontserratBold: Montserrat_700Bold
+  })
 
+  if (fontsLoaded) {
+    SplashScreen.hideAsync()
+  }
   if (!fontsLoaded) {
-    return <AppLoading />;
+    return null
   }
 
   return (
@@ -25,5 +28,5 @@ export default function App() {
       <StatusBar />
       <Basket {...mock} />
     </SafeAreaView>
-  );
+  )
 }
